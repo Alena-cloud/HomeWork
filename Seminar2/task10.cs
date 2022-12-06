2ff05e0c98bce7a24e65953a28923706cd51d981
Alena-cloud <118624395+Alena-cloud@users.noreply.github.com>
2022-12-06 15:25:38 -0600

    Create task10.cs

---
+++ Seminar2/task10.cs
@@ -0,0 +1,16 @@
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5, 782 -> 8, 918 -> 1
+
+Console.WriteLine("Enter three-digit number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if ((num1 >= 100) && (num1 < 1000))
+{
+    int firstDigit = num1 % 100;
+    int secondDigit = firstDigit / 10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Non-three-digit number has been entered");
+}
